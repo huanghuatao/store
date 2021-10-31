67d3d06655ce70822b4fa0e9029aa106f507bb3c
黄华涛 <93251256+huanghuatao@users.noreply.github.com>
2021-10-31 09:58:48 +0800

    Add files via upload

---
+++ 作业3（任务2）.py
@@ -0,0 +1,59 @@
+'''list_one = ['北京','上海','广东']
+list_two = ['天津','安徽','河南','海南','河北']
+list_one.append(list_two)
+print(list_one)'''#向列表添加数据
+
+'''def swapPositions(list, pos1, pos2):
+    first_ele = list.pop(pos1)
+    second_ele = list.pop(pos2 - 1)
+    list.insert(pos1, second_ele)
+    list.insert(pos2, first_ele)
+    return list
+List = ['北京','上海','广东']
+pos1, pos2 = 1, 3
+print(swapPositions(List, pos1 , pos2 - 1))'''#广东排在上海前面
+
+'''L=[36710.36,35427.10,29863.23,29667.39,27665.36,27650.45,27620.38,25369.20]
+
+a=sum(L)/len(L)
+
+print("avge is:", round(a,2) )'''#前8城市的GDP总和，平均GDP
+
+'''li =[1,2,3,4,5,6,7,8,9]
+a = list(reversed(li))
+print (a)'''#实现列表的数据翻转
+
+'''list =[1,4,7,5,8,2,1,3,4,5,9,7,6,1,10]
+a=list.count(1)
+b=list.count(2)
+c=list.count(3)
+d=list.count(4)
+e=list.count(5)
+f=list.count(6)
+g=list.count(7)
+h=list.count(8)
+i=list.count(9)
+j=list.count(10)
+print("数字1出现的次数：%d"%a)
+print("数字2出现的次数：%d"%b)
+print("数字3出现的次数：%d"%c)
+print("数字4出现的次数：%d"%d)
+print("数字5出现的次数：%d"%e)
+print("数字6出现的次数：%d"%f)
+print("数字7出现的次数：%d"%g)
+print("数字8出现的次数：%d"%h)
+print("数字9出现的次数：%d"%i)
+print("数字10出现的次数：%d"%j)'''#每个数字出现的次数
+
+
+
+
+
+
+
+
+
+
+
+
+
