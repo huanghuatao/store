1a36f23f8517be4ac68fd37a964585edd457b6ba
黄华涛 <93251256+huanghuatao@users.noreply.github.com>
2021-11-30 22:16:48 +0800

    Update E-mail.py

--- E-mail.py
+++ E-mail.py
@@ -1,34 +1,39 @@
 import smtplib
-from email.mime.text import MIMEText  # 文本
-from email.mime.multipart import MIMEMultipart  # 附件
-from email.header import Header  # 配置一些头信息
+import zmail
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 
-sender = "2236350998@qq.com"  # 发送人的邮件
-recevises = ["jason13552648187@sina.com"]  # 接收人列表，可以写多个
-mail_host = "smtp.qq.com"  # smtp邮箱的服务器地址
-password = "wzkysionostydjeg"  # 本人邮件发送授权码
-subject = "这是一封测试邮件"  # 本次邮件发送的主题
+# 第一种方法
+mail_host = "smtp.qq.com"  # 设置服务器
+mail_user = "2236350998@qq.com"  # 用户名
+mail_pass = "wzkysionostydjeg"  # 口令
 
-message = MIMEMultipart()  # 附件管理器，能添加N多个附件
-# 设置发送邮件的一些配置
-message["From"] = Header("Jason", "utf-8")
-message["TO"] = Header("测试", "utf-8")
-message["Subject"] = Header(subject, "utf-8")
+sender = '2236350998@qq.com'
+receivers = ['jason13552648187@sina.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
-message.attach(MIMEText("这是附件", "plain", "utf-8"))
-# 构造附件1，传送当前目录下的 test.txt 文件
-att1 = MIMEText(open('计算器.html', 'rb').read(), 'base64', 'utf-8')
+# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
+message = MIMEMultipart()
+message['From'] = Header('1', 'utf-8')  # 发送者
+
+message['To'] = Header("测试", 'utf-8')  # 接收者
+subject = '测试邮件'
+message['Subject'] = Header(subject, 'utf-8')
+
+message.attach(MIMEText('邮件发送测试……', 'plain', 'utf-8'))
+
+att1 = MIMEText(r'D:\Python学习\python作业\计算机\计算器.html', 'base64', 'utf-8')
 att1["Content-Type"] = 'application/octet-stream'
 # 这里的filename可以任意写，写什么名字，邮件中显示什么名字
-att1["Content-Disposition"] = 'attachment;filename="computer.html"'
+att1["Content-Disposition"] = 'attachment; filename="计算器.html"'
 message.attach(att1)
 
 try:
-    smtp = smtplib.SMTP()  # smtp 发送器
-    smtp.connect(mail_host, 587)
-    smtp.login(sender, password)
-    smtp.sendmail(sender, recevises, message.as_string())
-    print("发送成功！")
-except Exception as error:
-    print("邮件发送失败！", error)
+    smtpObj = smtplib.SMTP()
+    smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+    smtpObj.login(mail_user, mail_pass)
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException:
+    print('Error: 无法发送邮件')
 
